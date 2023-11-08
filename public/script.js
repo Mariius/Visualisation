3c77e11d331248de00f2a230ba7282794eeefd37
Marius Tiomouo <mariustiomouo@outlook.de>
2023-11-08 08:52:00 +0100

    Revert "Merge branch 'newtask' of github.com:Mariius/Visualisation into newtask"
    This reverts commit 623802c56719b2dc0ce92a24f9abbf47899431ff, reversing
    changes made to 588698732731e13d430fdb21bfe60d10479d794b.

--- public/script.js
+++ public/script.js
@@ -41,50 +41,50 @@
     console.log(myQuestions);
    
     const form = document.getElementById("dataForm");
-        const messageDiv = document.getElementById("message");
+    const messageDiv = document.getElementById("message");
 
-        form.addEventListener("submit", async (event) => {
-            event.preventDefault();
+    form.addEventListener("submit", async (event) => {
+      event.preventDefault();
 
-            const questionName = document.getElementById("questionName").value;
-            const questionText = document.getElementById("questionText").value;
-            const answer1Text = document.getElementById("answer1Text").value;
-            const answer1Points = parseInt(document.getElementById("answer1Points").value);
-            const answer1Correct = document.getElementById("answer1Correct").checked;
-            const answer2Text = document.getElementById("answer2Text").value;
-            const answer2Points = parseInt(document.getElementById("answer2Points").value);
-            const answer2Correct = document.getElementById("answer2Correct").checked;
-            const answer3Text = document.getElementById("answer3Text").value;
-            const answer3Points = parseInt(document.getElementById("answer3Points").value);
-            const answer3Correct = document.getElementById("answer3Correct").checked;
+      const questionName = document.getElementById("questionName").value;
+      const questionText = document.getElementById("questionText").value;
+      const answer1Text = document.getElementById("answer1Text").value;
+      const answer1Points = parseInt(document.getElementById("answer1Points").value);
+      const answer1Correct = document.getElementById("answer1Correct").checked;
+      const answer2Text = document.getElementById("answer2Text").value;
+      const answer2Points = parseInt(document.getElementById("answer2Points").value);
+      const answer2Correct = document.getElementById("answer2Correct").checked;
+      const answer3Text = document.getElementById("answer3Text").value;
+      const answer3Points = parseInt(document.getElementById("answer3Points").value);
+      const answer3Correct = document.getElementById("answer3Correct").checked;
 
-            const newData = {
-                "name": questionName,
-                "text": questionText,
-                "answers": [
-                    {
-                        "id": 0,
-                        "text": answer1Text,
-                        "points": answer1Points,
-                        "correct": answer1Correct,
-                        "percentage": answer1Correct ? 100 : 0
-                    },
-                    {
-                        "id": 1,
-                        "text": answer2Text,
-                        "points": answer2Points,
-                        "correct": answer2Correct,
-                        "percentage": answer2Correct ? 100 : 0
-                    },
-                    {
-                        "id": 2,
-                        "text": answer3Text,
-                        "points": answer3Points,
-                        "correct": answer3Correct,
-                        "percentage": answer3Correct ? 100 : 0
-                    }
-                ]
-            };
+      const newData = {
+          "name": questionName,
+          "text": questionText,
+          "answers": [
+            {
+              "id": 0,
+              "text": answer1Text,
+              "points": answer1Points,
+              "correct": answer1Correct,
+              "percentage": answer1Correct ? 100 : 0
+            },
+            {
+              "id": 1,
+              "text": answer2Text,
+              "points": answer2Points,
+              "correct": answer2Correct,
+              "percentage": answer2Correct ? 100 : 0
+            },
+            {
+              "id": 2,
+              "text": answer3Text,
+              "points": answer3Points,
+              "correct": answer3Correct,
+              "percentage": answer3Correct ? 100 : 0
+            }
+        ]
+      };
 
 
       fetch('/api/add', {
@@ -94,24 +94,24 @@
         },
         body: JSON.stringify(newData),
       })
-        .then(response => response.json())
-        .then(data => {
-          if (data.success) {
-            document.getElementById("questionName").value = '';
-            document.getElementById("questionText").value = '';
-            document.getElementById("answer1Text").value = '';
-            document.getElementById("answer1Points").value = '';
-            document.getElementById("answer1Correct").checked = false;
-            document.getElementById("answer2Text").value = '';
-            document.getElementById("answer2Points").value = '';
-            document.getElementById("answer2Correct").checked = false;
-            document.getElementById("answer3Text").value = '';
-            document.getElementById("answer3Points").value = '';
-            document.getElementById("answer3Correct").checked = false;
-            loadData();
-            
-          }
-        });
+      .then(response => response.json())
+      .then(data => {
+        if (data.success) {
+          document.getElementById("questionName").value = '';
+          document.getElementById("questionText").value = '';
+          document.getElementById("answer1Text").value = '';
+          document.getElementById("answer1Points").value = '';
+          document.getElementById("answer1Correct").checked = false;
+          document.getElementById("answer2Text").value = '';
+          document.getElementById("answer2Points").value = '';
+          document.getElementById("answer2Correct").checked = false;
+          document.getElementById("answer3Text").value = '';
+          document.getElementById("answer3Points").value = '';
+          document.getElementById("answer3Correct").checked = false;
+          loadData();
+          
+        }
+      });
 
     });
 
