c579eb8e34e2ab31aa0bfe7b961cab4a229ad18f
kamgang2 <kgb200076@yahoo.com>
2023-11-08 19:57:34 +0100

    some Question design

--- public/script.js
+++ public/script.js
@@ -8,121 +8,137 @@
 // }
 
 // --------------------load json data, add data to the json file -----------------------------------------------------------------------------------------------------------------
-    const dataList = document.getElementById('data-list');
-    const myQuestions= [];
+//const dataList = document.getElementById('data-table');
+const myQuestions = [];
 
-    // Funktion zum Laden der JSON-Daten und Anzeigen als Liste
-    function loadData() {
-      fetch('/api/data')
-        .then(response => response.json())
-        .then(data => {
-          dataList.innerHTML = '';
-          data.questions.forEach(question => {
-            const listItem = document.createElement('button');
-            listItem.id='buttonstyle';
-           // listItem.classList.add('transition', 'gradient','label'); 
-            const newline = document.createElement('br'); 
-            listItem.textContent = `${question.name}: ${question.text}`;
-            dataList.appendChild(listItem);
-            dataList.appendChild(newline); 
-            myQuestions.push(`${question.name}: ${question.text}`);
-            
+// Funktion zum Laden der JSON-Daten und Anzeigen als Liste
+var tbl = document.getElementById('data-table');
+tbl.style.width = '100%';
+tbl.setAttribute('border', '1');
+var tbdy = document.createElement('tbody');
+tbl.appendChild(tbdy);
 
-          });
-        });
-    }
-    
-    // console.log(myQuestions);
-   
-    const form = document.getElementById("dataForm");
-    const messageDiv = document.getElementById("message");
+function loadData() {
+  fetch('/api/data')
+    .then(response => response.json())
+    .then(data => {
+      data.questions.forEach(question => { 
+        //tabelle Elemente erstellen 
+        var tr = document.createElement('tr');
+        var td = document.createElement('td');; 
+        td.textContent = `${question.name}`; // Fragebezeichnung als Textinhalt jede Zelle zuweisen
+        tr.appendChild(td);
+        tbdy.appendChild(tr);
 
-    form.addEventListener("submit", async (event) => {
-      event.preventDefault();
+        // tr.onclick = function(){
+         
+        // }
+      
 
-      const questionName = document.getElementById("questionName").value;
-      const questionText = document.getElementById("questionText").value;
-      const answer1Text = document.getElementById("answer1Text").value;
-      const answer1Points = parseInt(document.getElementById("answer1Points").value);
-      const answer1Correct = document.getElementById("answer1Correct").checked;
-      const answer2Text = document.getElementById("answer2Text").value;
-      const answer2Points = parseInt(document.getElementById("answer2Points").value);
-      const answer2Correct = document.getElementById("answer2Correct").checked;
-      const answer3Text = document.getElementById("answer3Text").value;
-      const answer3Points = parseInt(document.getElementById("answer3Points").value);
-      const answer3Correct = document.getElementById("answer3Correct").checked;
+        tr.onmouseover = function(){
+          td.textContent=`${question.text}`;
+         }
 
-      const newData = {
-          "name": questionName,
-          "text": questionText,
-          "answers": [
-            {
-              "id": 0,
-              "text": answer1Text,
-              "points": answer1Points,
-              "correct": answer1Correct,
-              "percentage": answer1Correct ? 100 : 0
-            },
-            {
-              "id": 1,
-              "text": answer2Text,
-              "points": answer2Points,
-              "correct": answer2Correct,
-              "percentage": answer2Correct ? 100 : 0
-            },
-            {
-              "id": 2,
-              "text": answer3Text,
-              "points": answer3Points,
-              "correct": answer3Correct,
-              "percentage": answer3Correct ? 100 : 0
-            }
-        ]
-      };
-
-
-      fetch('/api/add', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(newData),
-      })
-      .then(response => response.json())
-      .then(data => {
-        if (data.success) {
-          document.getElementById("questionName").value = '';
-          document.getElementById("questionText").value = '';
-          document.getElementById("answer1Text").value = '';
-          document.getElementById("answer1Points").value = '';
-          document.getElementById("answer1Correct").checked = false;
-          document.getElementById("answer2Text").value = '';
-          document.getElementById("answer2Points").value = '';
-          document.getElementById("answer2Correct").checked = false;
-          document.getElementById("answer3Text").value = '';
-          document.getElementById("answer3Points").value = '';
-          document.getElementById("answer3Correct").checked = false;
-          loadData();
-          
-        }
+        tr.onmouseleave = function(){
+      
+             td.textContent=`${question.name}`;
+         }
+        
+        myQuestions.push(`${question.name}: ${question.text}`);
       });
+    });
+}
 
+// console.log(myQuestions);
+
+const form = document.getElementById("dataForm");
+const messageDiv = document.getElementById("message");
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const questionName = document.getElementById("questionName").value;
+  const questionText = document.getElementById("questionText").value;
+  const answer1Text = document.getElementById("answer1Text").value;
+  const answer1Points = parseInt(document.getElementById("answer1Points").value);
+  const answer1Correct = document.getElementById("answer1Correct").checked;
+  const answer2Text = document.getElementById("answer2Text").value;
+  const answer2Points = parseInt(document.getElementById("answer2Points").value);
+  const answer2Correct = document.getElementById("answer2Correct").checked;
+  const answer3Text = document.getElementById("answer3Text").value;
+  const answer3Points = parseInt(document.getElementById("answer3Points").value);
+  const answer3Correct = document.getElementById("answer3Correct").checked;
+
+  const newData = {
+    "name": questionName,
+    "text": questionText,
+    "answers": [
+      {
+        "id": 0,
+        "text": answer1Text,
+        "points": answer1Points,
+        "correct": answer1Correct,
+        "percentage": answer1Correct ? 100 : 0
+      },
+      {
+        "id": 1,
+        "text": answer2Text,
+        "points": answer2Points,
+        "correct": answer2Correct,
+        "percentage": answer2Correct ? 100 : 0
+      },
+      {
+        "id": 2,
+        "text": answer3Text,
+        "points": answer3Points,
+        "correct": answer3Correct,
+        "percentage": answer3Correct ? 100 : 0
+      }
+    ]
+  };
+
+
+  fetch('/api/add', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newData),
+  })
+    .then(response => response.json())
+    .then(data => {
+      if (data.success) {
+        document.getElementById("questionName").value = '';
+        document.getElementById("questionText").value = '';
+        document.getElementById("answer1Text").value = '';
+        document.getElementById("answer1Points").value = '';
+        document.getElementById("answer1Correct").checked = false;
+        document.getElementById("answer2Text").value = '';
+        document.getElementById("answer2Points").value = '';
+        document.getElementById("answer2Correct").checked = false;
+        document.getElementById("answer3Text").value = '';
+        document.getElementById("answer3Points").value = '';
+        document.getElementById("answer3Correct").checked = false;
+        loadData();
+
+      }
     });
 
-    // Initial laden der Daten
-    loadData();
+});
 
-    // -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Initial laden der Daten
+loadData();
 
-    function showContent(contentId) {
-      // Alle Inhaltscontainer ausblenden
-      var contentContainers = document.querySelectorAll('.inhalt_recht');
-      contentContainers.forEach(function (container) {
-          container.style.display = 'none';
-      });
-  
-      // Den ausgewählten Inhalt anzeigen
-      var selectedContent = document.getElementById(contentId);
-      selectedContent.style.display = 'block';
-  }
-  
+// -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+function showContent(contentId) {
+  // Alle Inhaltscontainer ausblenden
+  var contentContainers = document.querySelectorAll('.inhalt_recht');
+  contentContainers.forEach(function (container) {
+    container.style.display = 'none';
+  });
+
+  // Den ausgewählten Inhalt anzeigen
+  var selectedContent = document.getElementById(contentId);
+  selectedContent.style.display = 'block';
+}
