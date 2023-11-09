796a536f8e2e4ed84a7b2b7ba0aec92a67dcbc9b
kamgang2 <kgb200076@yahoo.com>
2023-11-09 18:28:07 +0100

    New  awful style but new feature at cell's button

--- public/script.js
+++ public/script.js
@@ -9,63 +9,78 @@
 
 // --------------------load json data, add data to the json file -----------------------------------------------------------------------------------------------------------------
 //const dataList = document.getElementById('data-table');
-const myQuestions = [];
+const myQuestions = new Map();
+const questions = new Map();// hold all data from JSON-file an store it here
 
-    // Funktion zum Laden der JSON-Daten und Anzeigen als Liste
-    var tbl = document.getElementById('data-table');
-    tbl.style.width = '100%';
-    tbl.setAttribute('border', '1');
-    var tbdy = document.createElement('tbody');
-    tbl.appendChild(tbdy);
+// Funktion zum Laden der JSON-Daten und Anzeigen als Liste
+var tbl = document.getElementById('data-table');
+tbl.style.width = '100%';
+//tbl.setAttribute('border', '1');
+tbl.setAttribute('borderRadius', '20px');
+var tbdy = document.createElement('tbody');
+tbl.appendChild(tbdy);
 
-    function loadData() {
-      fetch('/api/data')
-        .then(response => response.json())
-        .then(data => {
-          
-          data.questions.forEach(question => {
+function loadData() {
+  fetch('/api/data')
+    .then(response => response.json())
+    .then(data => {
 
-          //tabelle Elemente erstellen 
-          var tr = document.createElement('tr');
-          var td = document.createElement('td');; 
-          var buttonItem= document.createElement('button');
-          buttonItem.textContent = `${question.name}`; // Fragebezeichnung als Textinhalt jede Zelle zuweisen
-          td.appendChild(buttonItem); 
-          tr.appendChild(td);
-          tbdy.appendChild(tr);
+      data.questions.forEach(question => {
 
-            // const listItem = document.createElement('button');
-          
-           // listItem.classList.add('transition', 'gradient','label'); 
-            // const newline = document.createElement('br'); 
-            // listItem.textContent = `${question.name}: ${question.text}`;
-            // dataList.appendChild(listItem);
-            // dataList.appendChild(newline); 
-
-            tr.onmouseover = function(){
-              td.textContent=`${question.text}`;
-              td.style.backgroundColor="green" ; 
-             }
-    
-            tr.onmouseleave = function(){
-          
-                 td.textContent=`${question.name}`;
-                 td.style.backgroundColor="#15a0af";
-             }
-            
-            myQuestions.push(`${question.name}: ${question.text}`);
-            
-
+        //tabelle Elemente erstellen 
+        var tr = document.createElement('tr');
+        var td = document.createElement('td');;
+        var buttonItem = document.createElement('button');
+        buttonItem.id = question.name;
+        buttonItem.textContent = `${question.name}`; // Fragebezeichnung als Textinhalt jede Zelle zuweisen
+        td.appendChild(buttonItem);
+        tr.appendChild(td);
+        tbdy.appendChild(tr);
+        // The Mouse over some Question will change the style
+        tr.onmouseover = function () {
+          td.textContent = `${question.text}`;
+          td.style.backgroundColor = "skyblue";
+        }
+        
+        // The Style will return to the initial style
+        tr.onmouseleave = function () {
+          td.textContent = `${question.name}`;
+          td.style.backgroundColor = "#15a0af";
+        }
+        // loading all data  in our maps
+        myQuestions.set(question.name, question.text);
+        questions.set(question.name, question);
+        //  show Answer's details when we click on some Questions
+        tr.onmousedown = function () {
+          const currentQuestion = questions.get(buttonItem.id)
+          var questionResponses = document.getElementById('questionResponses');
+          questionResponses.innerHTML = '';
+          var label = document.createElement('label');
+          for(let i= 0; i<currentQuestion.answers.length; i++ )
+          {
+            label.innerHTML += currentQuestion.answers[i].text+' ' ;
+          }
+          questionResponses.appendChild(label);
+          // Alle Inhaltscontainer ausblenden
+          //  Dies habe ich von Mario kopiert
+          var contentContainers = document.querySelectorAll('.inhalt_recht');
+          contentContainers.forEach(function (container) {
+            container.style.display = 'none';
           });
-        });
-    }
-    
-    // console.log(myQuestions);
-   
-    const form = document.getElementById("dataForm");
-    const messageDiv = document.getElementById("message");
-    const newQ = document.getElementById("newQuestion");
-    const startR1 = document.getElementById("start");
+          // Den ausgewählten Inhalt anzeigen
+          questionResponses.style.display = 'block';
+        }
+
+      });
+    });
+}
+
+console.log(myQuestions);
+
+const form = document.getElementById("dataForm");
+const messageDiv = document.getElementById("message");
+const newQ = document.getElementById("newQuestion");
+const startR1 = document.getElementById("start");
 
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
@@ -133,27 +148,27 @@ form.addEventListener("submit", async (event) => {
         document.getElementById("answer3Points").value = '';
         document.getElementById("answer3Correct").checked = false;
         loadData();
-        const done=document.createElement('p');
-        done.textContent=`the question ${questionName} was added successfuly`;
+        const done = document.createElement('p');
+        done.textContent = `the question ${questionName} was added successfuly`;
         messageDiv.appendChild(done);
 
-        setTimeout(function() {
+        setTimeout(function () {
           messageDiv.style.display = "none";
           newQ.style.display = "none";
-          startR1.style.display ="flex";
+          startR1.style.display = "flex";
         }, 100); // 10000 Millisekunden (10 Sekunden)
 
       }
-      else{
-        const failled=document.createElement('p');
-        failled.textContent=`the question ${questionName} could not be added`;
+      else {
+        const failled = document.createElement('p');
+        failled.textContent = `the question ${questionName} could not be added`;
         messageDiv.appendChild(failled);
 
-        
-        setTimeout(function() {
+
+        setTimeout(function () {
           messageDiv.style.display = "none";
           newQ.style.display = "none";
-          startR1.style.display ="flex";
+          startR1.style.display = "flex";
         }, 100); // 10000 Millisekunden (10 Sekunden)
 
       }
@@ -166,20 +181,20 @@ loadData();
 
 // -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    function showContent(contentId) {
-      // Alle Inhaltscontainer ausblenden
-      var contentContainers = document.querySelectorAll('.inhalt_recht');
-      contentContainers.forEach(function (container) {
-          container.style.display = 'none';
-      });
-  
-      // Den ausgewählten Inhalt anzeigen
-      var selectedContent = document.getElementById(contentId);
-      selectedContent.style.display = 'block';
-  }
-  
-  //  Details einer Frage anzeigen
+function showContent(contentId) {
+  // Alle Inhaltscontainer ausblenden
+  var contentContainers = document.querySelectorAll('.inhalt_recht');
+  contentContainers.forEach(function (container) {
+    container.style.display = 'none';
+  });
 
-  const details = document.getElementById("questionDetails");
-  details.innerHTML = ` `;
-  const dItem=createElement("li");
+  // Den ausgewählten Inhalt anzeigen
+  var selectedContent = document.getElementById(contentId);
+  selectedContent.style.display = 'block';
+}
+
+//  Details einer Frage anzeigen
+
+const details = document.getElementById("questionDetails");
+details.innerHTML = ` `;
+//const dItem=createElement("li");
