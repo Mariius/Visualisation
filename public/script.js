a10fd9bf8ce465068da336a3b2270f2e52103d34
Marius Tiomouo <mariustiomouo@outlook.de>
2023-12-02 17:10:54 +0100

    some tests

--- public/script.js
+++ public/script.js
@@ -4,15 +4,23 @@ function loadData() {
     .then(data => {
 
       // create a GOjs-Diagramm
-      var $ = go.GraphObject.make;
-
+      var $ = go.GraphObject.make; 
+// --------------------force directed aus tutorial---------------------------------------
+      // var Diagram =
+      //   new go.Diagram("quiz-container", {
+      //     layout: $(go.ForceDirectedLayout, {
+      //       defaultSpringLength: 0,
+      //       defaultElectricalCharge: 50
+      //     })
+      //   });
+// --------------------------------------------------------------------------------------
       var Diagram =
-        new go.Diagram("quiz-container", {
-          layout: $(go.ForceDirectedLayout, {
-            defaultSpringLength: 0,
-            defaultElectricalCharge: 50
-          })
-        });
+      new go.Diagram("quiz-container", {
+        layout: $(ContinuousForceDirectedLayout,  // automatically spread nodes apart while dragging
+          { defaultSpringLength: 0, defaultElectricalCharge: 50 }),
+        // do an extra layout at the end of a move
+        "SelectionMoved": e => e.diagram.layout.invalidateLayout()
+      });  
 
       // Define node templates for question and answer nodes
       Diagram.nodeTemplate =
@@ -451,3 +459,42 @@ document.addEventListener("DOMContentLoaded", function () {
 //   });
 // }
 
+// --------------------------- class für force dirested aus GO.js tutorial---------------------------------------------------------------------------
+class ContinuousForceDirectedLayout extends go.ForceDirectedLayout {
+  isFixed(v) {
+    return v.node.isSelected;
+  }
+
+  // optimization: reuse the ForceDirectedNetwork rather than re-create it each time
+  doLayout(coll) {
+    if (!this._isObserving) {
+      this._isObserving = true;
+      // cacheing the network means we need to recreate it if nodes or links have been added or removed or relinked,
+      // so we need to track structural model changes to discard the saved network.
+      this.diagram.addModelChangedListener(e => {
+        // modelChanges include a few cases that we don't actually care about, such as
+        // "nodeCategory" or "linkToPortId", but we'll go ahead and recreate the network anyway.
+        // Also clear the network when replacing the model.
+        if (e.modelChange !== "" ||
+          (e.change === go.ChangedEvent.Transaction && e.propertyName === "StartingFirstTransaction")) {
+          this.network = null;
+        }
+      });
+    }
+    var net = this.network;
+    if (net === null) {  // the first time, just create the network as normal
+      this.network = net = this.makeNetwork(coll);
+    } else {  // but on reuse we need to update the LayoutVertex.bounds for selected nodes
+      this.diagram.nodes.each(n => {
+        var v = net.findVertex(n);
+        if (v !== null) v.bounds = n.actualBounds;
+      });
+    }
+    // now perform the normal layout
+    super.doLayout(coll);
+    // doLayout normally discards the LayoutNetwork by setting Layout.network to null;
+    // here we remember it for next time
+    this.network = net;
+  }
+}
+// end ContinuousForceDirectedLayout ------------------------------------------------------------------------------------------------------------
